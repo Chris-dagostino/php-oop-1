b4c4de6ddde9218133669a176b8a8c6616c453dd
Chris-dagostino <dagostino.christian94@gmail.com>
2022-03-15 17:29:51 +0100

    implemento getDurata

--- index.php
+++ index.php
@@ -9,23 +9,35 @@
         public $durata;
 
         //=> all’interno della classe è definito un costruttore
-        public function __construct($titolo, $genere, $orario, $durata)
+        public function __construct($titolo, $genere, $orario)
         {
             $this->titolo = $titolo;
             $this->genere = $genere;
             $this->orario = $orario;
-            $this->durata = $durata;
+            
         }
 
         //=> all’interno della classe è definito almeno un metodo
-        public function setDurata (){
-            $this->durata += 20;
+        public function setDurata ($tempo){
+            $this->durata = $tempo + 20;
+        }
+
+        public function getDurata() {
+            return $this->durata;
         }
     }
 
     //- vengono istanziati almeno due oggetti ‘Movie’
-    $guardianiDellaGalassia = new movie ('guardiani della galassia','fantascienza','20.00','1.50');
-    $uncharted = new movie ('uncharted','azione','21.30','1.45');
+    $guardianiDellaGalassia = new movie ('guardiani della galassia','fantascienza','20.00');
+    $uncharted = new movie ('uncharted','azione','21.30');
+
+
+    $durata_guardianiDellaGalassia = $guardianiDellaGalassia->getDurata();
+    $durata_uncharted = $uncharted->getDurata();
+     
+
+    $guardianiDellaGalassia->setDurata(50);
+    $uncharted->setDurata(45); 
 
     //stampati a schermo i valori delle relative proprietà
     var_dump($guardianiDellaGalassia);
@@ -42,20 +54,22 @@
 </head>
 <body>
 
-    <?php
-        foreach ($guardianiDellaGalassia as $data ) {
-            echo '<h1>'. $data["titolo"]  . '</h1>' . '<br>';
-            echo '<p>'. $data["genere"]  . '</p>';
-            echo '<p>'. $data["orario"]  . '</p>';
-            echo '<p>'. $data["durata"]  . '</p>';
-        }
 
-        foreach ($uncharted as $data ) {
-            echo '<h1>'. $data["titolo"]  . '</h1>' . '<br>';
-            echo '<p>'. $data["genere"]  . '</p>';
-            echo '<p>'. $data["orario"]  . '</p>';
-            echo '<p>'. $data["durata"]  . '</p>';
-        }
+    <?php
+
+
+        echo "<h1>" . $guardianiDellaGalassia->titolo  . "</h1>";
+        echo "<h1>" . $guardianiDellaGalassia->genere  . "</h1>";
+        echo "<h1>" . $guardianiDellaGalassia->orario  . "</h1>";
+        echo "<h1>" . $guardianiDellaGalassia->durata  . "</h1>";
+
+        echo "<hr>";
+
+        echo "<h1>" . $uncharted->titolo  . "</h1>";
+        echo "<h1>" . $uncharted->genere  . "</h1>";
+        echo "<h1>" . $uncharted->orario  . "</h1>";
+        echo "<h1>" . $uncharted->durata  . "</h1>";
+
     ?>
     
 </body>
